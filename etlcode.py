533f4b43d9f7be10ffedda03edbe4fb00a172248
Halianna <117320191+hpiller22@users.noreply.github.com>
2025-10-16 18:36:44 -0700

    Update API request and data processing in etlcode.py

--- etlcode.py
+++ etlcode.py
@@ -2,8 +2,10 @@ import pandas as pd
 import requests
 from datetime import datetime
 
-az511_url = #redacted for privacy
-#code get response redacted for privacy
+az511_url = [api url]
+
+params = {"key": [api key] }
+response = requests.get([api_url]", params=params)
 
 if response.status_code == 200:
     print("Request was successful!")
@@ -12,7 +14,8 @@ else:
 
 data = response.json()
 df = pd.DataFrame(data)
+df["Reported"] = pd.to_numeric(df["Reported"], errors="coerce")
+df = df[df["Reported"].notna() & (df["Reported"] > 0)]
 
-#unix time conversion
 unix_time = df["Reported"].astype(int)
 df["ReportTime"] = df["Reported"].astype(int).apply(datetime.fromtimestamp)
